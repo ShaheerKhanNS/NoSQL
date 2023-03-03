71680e640156a8e2b5106e7e54dbde3e7b7bb27a
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-03-03 10:37:22 +0530

    Added orders controller

--- models/user.js
+++ models/user.js
@@ -84,6 +84,39 @@ class User {
       .catch((err) => console.log(err));
   }
 
+  addOrder() {
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new mongoDb.ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
+
+      .then((res) => {
+        this.cart = { items: [] };
+        return db
+          .collection("users")
+          .updateOne(
+            { _id: new mongoDb.ObjectId(this.id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection("orders")
+      .find({ "users._id": new mongoDb.ObjectId(this.id) })
+      .toArray();
+  }
+
   deleteItemFromCart(productId) {
     const updatedCartItems = this.cart.items.filter((item) => {
       return item.productId.toString() !== productId.toString();
